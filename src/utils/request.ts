0bc968c83f85dd8f807d935400055b1c2ac4c2e8
Kylin <1159469891@qq.com>
2024-03-22 17:35:23 +0800

    feat: 添加axios、cache

---
+++ src/utils/request.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const request = axios.create()
+
+// request interceptor
+request.interceptors.request.use(
+  (config) => {
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+request.interceptors.response.use(
+  (response) => {
+    if (response.status !== 200 && response.status !== 201) {
+      return Promise.reject(response)
+    } else {
+      const data: any = response.data
+      if (data.success) {
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    }
+  },
+  async (error) => {
+    return Promise.reject(error)
+  }
+)
+
+export default request
